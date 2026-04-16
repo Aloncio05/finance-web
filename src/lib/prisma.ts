854cd19e5b1437f839384e2c0a8c86df0b9116a6
Aloncio Martins da Costa Junior <155736258+Aloncio05@users.noreply.github.com>
2026-04-16 11:03:17 -0300

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,24 +1,10 @@
-import { PrismaLibSql } from "@prisma/adapter-libsql";
-
 import { PrismaClient } from "@/generated/prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
+const globalForPrisma = globalThis as {
   prisma?: PrismaClient;
 };
 
-const connectionString = process.env.DATABASE_URL;
-const authToken = process.env.TURSO_AUTH_TOKEN;
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL não configurada.");
-}
-
-const adapter = new PrismaLibSql({
-  url: connectionString,
-  authToken,
-});
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
